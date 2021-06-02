baf2202a34c775f2ef1b143686255e39c964447f
Tech <sesor333@aol.com>
2021-06-02 16:26:48 -0400

    Initial Commit

---
+++ src/discordBot.py
@@ -0,0 +1,159 @@
+import discord
+import youtube_dl
+import os
+from discord.ext import commands
+from discord.utils import get
+from discord import FFmpegPCMAudio
+from os import system
+from secret import private as p
+#Remember to pip discord[voice]
+
+client = commands.Bot(command_prefix='!')
+
+
+
+@client.event
+async def on_ready():
+    print('Logged in as ' + client.user.name)
+    print(client.user.id)
+    print('All systems online!')
+
+@client.command()
+async def avatar(ctx):
+    url = ctx.message.author.avatar_url
+    await ctx.send(str(url))
+
+@client.command()
+async def whoami(ctx):
+    await ctx.send('You are ' + str(ctx.message.author))
+
+@client.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+@client.command()
+async def twitch(ctx):
+    await ctx.send('Follow me on Twitch at https://www.twitch.tv/Tech_Coyote')
+
+@client.command()
+async def version(ctx):
+    clientName = client.user.name
+    server = ctx.message.author.guild.name
+    version = 0.2
+    await ctx.send(str(clientName) + " version " + str(version))
+    await ctx.send("Running on the Server: " + str(server))
+
+#Voice
+
+#@client.command()
+#async def join(ctx):
+#    channel = ctx.message.author.voice.channel
+#    vc = await channel.connect()
+
+@client.command(pass_context=True, brief="Makes the bot join your channel", aliases=['j', 'jo','connect'])
+async def join(ctx):
+    channel = ctx.message.author.voice.channel
+    if not channel:
+        await ctx.send("You are not connected to a voice channel")
+        return
+    voice = get(client.voice_clients, guild=ctx.guild)
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+    await ctx.send(f"Joined {channel}")
+
+@client.command(pass_context=True, brief="Makes the bot leave your channel", aliases=['dc','leave'])
+async def disconnect(ctx):
+    await client.voice_clients[0].disconnect()
+
+@client.command(pass_context=True, brief="This will play a song 'play [url]'", aliases=['pl','p'])
+async def play(ctx, url: str):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+    except PermissionError:
+        await ctx.send("Wait for the current playing music end or use the 'stop' command")
+        return
+    await ctx.send("Getting everything ready, playing audio soon")
+    print("Someone wants to play music let me get that ready for them...")
+    voice = get(client.voice_clients, guild=ctx.guild)
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, 'song.mp3')
+    voice = get(client.voice_clients, guild=ctx.guild)
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+    voice.volume = 100
+    voice.is_playing()
+
+@client.command(pass_context=True, brief="Repeats last song played", aliases=['playlast','again'])
+async def repeat(ctx):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            voice = get(client.voice_clients, guild=ctx.guild)
+            voice.play(discord.FFmpegPCMAudio("song.mp3"))
+            voice.volume = 100
+            voice.is_playing()
+    except:
+        await ctx.send("Nothing is in buffer, play a song first")
+        return
+
+@client.command(pass_context=True, brief="Pauses currently playing audio")
+async def pause(ctx):
+    voice = get(client.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+
+@client.command(pass_context=True, brief="Stops currently playing audio")
+async def stop(ctx):
+    voice = get(client.voice_clients, guild=ctx.guild)
+    if voice.is_playing() or voice.is_paused():
+        voice.stop()
+
+@client.command(pass_context=True, brief="Resumes paused audio",aliases=['unpause'])
+async def resume(ctx):
+    voice = get(client.voice_clients, guild=ctx.guild)
+    if voice.is_paused():
+        voice.resume()
+
+@client.command(pass_context=True, brief="Changes to volume",aliases=['vol','v'])
+async def volume(ctx, vol: int):
+    voice = get(client.voice_clients, guild=ctx.guild)
+    if voice.is_playing() or voice.is_paused():
+        voice.volume = vol
+#Reference commands
+
+"""
+@client.command()
+async def voicechan(ctx):
+    channel = ctx.message.author.voice.channel
+    await ctx.send(str(channel))
+"""
+
+"""
+@client.command()
+async def test(ctx):
+    user = ctx.message.author
+    await ctx.send(str(user))
+"""
+
+"""
+@client.command()
+async def square(ctx, num):
+    val = int(num) * int(num)
+    await ctx.send(str(num) + ' squared is ' + str(val))
+"""
+
+client.run(p.botToken)
