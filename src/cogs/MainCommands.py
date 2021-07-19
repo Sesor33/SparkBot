d860a2a951bcd06ab858ac07639537521fb53824
Tech <sesor333@aol.com>
2021-07-19 15:43:37 -0400

    Steam API implemented

--- src/cogs/MainCommands.py
+++ src/cogs/MainCommands.py
@@ -6,57 +6,57 @@ class MainCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(brief="Gets the avatar URL of a member")
+    @commands.command(brief='Gets the avatar URL of a member')
     async def avatar(self, ctx, member : discord.Member):
-        await ctx.send(f"{member.avatar_url}")
+        await ctx.send(f'{member.avatar_url}')
 
-    @commands.command(brief="Test command, says who YOU are")
+    @commands.command(brief='Test command, says who YOU are')
     async def whoami(self, ctx):
         await ctx.send('\u180EYou are ' + str(self, ctx.message.author))
 
-    @commands.command(brief="Ping? Pong.")
+    @commands.command(brief='Ping? Pong.')
     async def ping(self, ctx):
         await ctx.send('\u180Epong')
 
-    @commands.command(brief="Shouts out your Twitch link")
+    @commands.command(brief='Shouts out your Twitch link')
     async def twitch(self, ctx):
         await ctx.send('\u180EFollow me on Twitch at https://www.twitch.tv/Tech_Coyote')
 
-    @commands.command(brief="Gives info about SparkBot")
+    @commands.command(brief='Gives info about SparkBot')
     async def info(self, ctx):
         version = 0.3
-        embed = discord.Embed(title = "SparkBot")
+        embed = discord.Embed(title = 'SparkBot')
         embed.set_thumbnail(url = client.user.avatar_url)
-        embed.add_field(name = "Version", value = f"{version}", inline = True)
-        embed.add_field(name = "Info", value = f"Running on the Server: {ctx.message.author.guild.name}", inline = False)
-        embed.add_field(name = "Stats", value = f"Discord API Latency: {(client.latency * 1000):.2f}ms", inline = False)
-        embed.set_footer(icon_url = ctx.author.avatar_url, text = f"Queried by {ctx.author.display_name}")
+        embed.add_field(name = 'Version', value = f'{version}', inline = True)
+        embed.add_field(name = 'Info', value = f'Running on the Server: {ctx.message.author.guild.name}', inline = False)
+        embed.add_field(name = 'Stats', value = f'Discord API Latency: {(client.latency * 1000):.2f}ms', inline = False)
+        embed.set_footer(icon_url = ctx.author.avatar_url, text = f'Queried by {ctx.author.display_name}')
         await ctx.send(embed=embed)
 
-    @commands.command(brief="Gets info on a member")
+    @commands.command(brief='Gets info on a member')
     async def whois(self, ctx, member : discord.Member):
         embed = discord.Embed(title = member.name, description = member.mention, color = discord.Colour.blue())
         embed.set_thumbnail(url = member.avatar_url)
-        embed.add_field(name = "ID", value = member.id, inline = True)
-        embed.add_field(name = "Role", value = member.top_role.mention, inline = True)
-        embed.set_footer(icon_url = ctx.author.avatar_url, text = f"Queried by {ctx.author.display_name}" )
+        embed.add_field(name = 'ID', value = member.id, inline = True)
+        embed.add_field(name = 'Role', value = member.top_role.mention, inline = True)
+        embed.set_footer(icon_url = ctx.author.avatar_url, text = f'Queried by {ctx.author.display_name}')
         await ctx.send(embed=embed)
 
-    @commands.command(brief="Kicks member")
+    @commands.command(brief='Kicks member')
     @commands.has_permissions(kick_members = True)
-    async def kick(self, ctx,member : discord.Member,*,reason= "No reason given"):
-        embed = discord.Embed(title = "Moderation", description = "Kicked "+member.name, color = discord.Colour.red())
-        embed.add_field(name = "ID", value = member.id, inline = True)
-        await member.send(f"You have been kicked for: {reason}")
+    async def kick(self, ctx,member : discord.Member,*,reason= 'No reason given'):
+        embed = discord.Embed(title = 'Moderation', description = 'Kicked '+member.name, color = discord.Colour.red())
+        embed.add_field(name = 'ID', value = member.id, inline = True)
+        await member.send(f'You have been kicked for: {reason}')
         await member.kick(reason=reason)
         await ctx.send(embed=embed)
 
-    @commands.command(brief="Bans member")
+    @commands.command(brief='Bans member')
     @commands.has_permissions(ban_members = True)
-    async def ban(self, ctx,member : discord.Member,*,reason= "No reason given"):
-        embed = discord.Embed(title = "Moderation", description = "Banned "+member.name, color = discord.Colour.red())
-        embed.add_field(name = "ID", value = member.id, inline = True)
-        await member.send(f"You have been banned for: {reason}")
+    async def ban(self, ctx,member : discord.Member,*,reason= 'No reason given'):
+        embed = discord.Embed(title = 'Moderation', description = 'Banned '+member.name, color = discord.Colour.red())
+        embed.add_field(name = 'ID', value = member.id, inline = True)
+        await member.send(f'You have been banned for: {reason}')
         await member.ban(reason=reason)
         await ctx.send(embed=embed)
 
--- src/cogs/Steam.py
+++ src/cogs/Steam.py
@@ -8,17 +8,49 @@ from discord.utils import get
 class Steam(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.steamSpyAPIURL = 'https://steamspy.com/api.php'
+        self.steamCurrPlayerAPIURL = 'http://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v0001/?appid='
 
-    @commands.command(brief="Gets the avatar URL of a member")
-    async def steamtest(self, ctx):
-        await ctx.send("Steam module is online")
 
     @commands.command(brief="Gets the player numbers for a steam game based on AppID")
     async def playercount(self, ctx, appID : str):
         async with aiohttp.ClientSession() as session:
-            async with session.get('http://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v0001/?appid=' + appID) as request:
+            async with session.get(self.steamCurrPlayerAPIURL + appID) as request:
                 if request.status == 200:
                     data = await request.json()
                     await ctx.send(f"Current players: {data['response']['player_count']}")
+                else:
+                    await ctx.send(f"Error, AppID: {appID} not found")
+
+    @commands.command(brief="Gets the info for a steam game based on AppID", aliases=['gs', 'steamstats'])
+    async def gamestats(self, ctx, appID : str):
+        steamSpyRequestURL = f'{self.steamSpyAPIURL}?request=appdetails&appid={appID}'
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.steamCurrPlayerAPIURL + appID) as playerRequest:
+                if playerRequest.status == 200:
+                    playerCountData = await playerRequest.json()
+                else:
+                    await ctx.send(f"Error, AppID: {appID} not found")
+
+            #TODO: Error handling outside of saying app not found        
+            async with session.get(steamSpyRequestURL) as request:
+                if request.status == 200:
+                    data = await request.json()
+
+                    embed = discord.Embed(title = data['name'], description = f'AppID: {appID}', color = discord.Colour.blue())
+                    embed.set_footer(icon_url = ctx.author.avatar_url, text = f'Queried by {ctx.author.display_name}')
+                    embed.set_thumbnail(url = f'http://cdn.akamai.steamstatic.com/steam/apps/{appID}/header.jpg')
+                    embed.add_field(name = 'Developer', value = data['developer'] , inline = True)
+                    embed.add_field(name = 'Publisher', value = data['publisher'] , inline = True)
+                    embed.add_field(name = 'Price', value = f"${float(data['price'])/100:.2f}" , inline = True)
+                    embed.add_field(name = 'Current Players', value = playerCountData['response']['player_count'], inline = True)
+                    embed.add_field(name = 'Owners', value = data['owners'], inline = True)
+                    embed.add_field(name = 'Store Link', value = f'http://store.steampowered.com/app/{appID}', inline = False)
+
+                    await ctx.send(embed=embed)
+                else:
+                    await ctx.send(f"Error, AppID: {appID} not found")
+
+
 def setup(bot):
     bot.add_cog(Steam(bot))
